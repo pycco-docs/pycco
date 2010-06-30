993fbde068c7cad977aa2be857237f6ab7d018f1
Nick Fitzgerald <fitzgen@gmail.com>
2010-06-29 21:18:53 -0700

    Fixing up pocco to work with files that dont have an extension and adding the setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="Pocco",
+      version="0.1",
+      description="""A Python port of Docco: the original quick-and-dirty, hundred-line-long,
+      literate-programming-style documentation generator.""",
+      author="Nick Fitzgerald",
+      author_email="fitzgen@gmail.com",
+      url="http://fitzgen.github.com/pocco",
+      scripts=["pocco"])
