45eb989f4283210e8abe11d8bd1ccc425284acba
Nick Fitzgerald <fitzgen@gmail.com>
2010-07-08 09:59:49 -0700

    Renaming Pocco to Pycco

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name="Pocco",
+setup(name="Pycco",
       version="0.1",
       description="""A Python port of Docco: the original quick-and-dirty, hundred-line-long,
       literate-programming-style documentation generator.""",
       author="Nick Fitzgerald",
       author_email="fitzgen@gmail.com",
       url="http://fitzgen.github.com/pocco",
-      scripts=["pocco"])
+      scripts=["pycco"])
