347356715b691024ee2aac09e7221ec14056ef39
Trey Hunner <treyhunner@gmail.com>
2011-09-30 13:27:16 -0700

    Remove test case (accidently merged in #27)
    Removes test case added in fa778aab02f29a1371790088b58d3a5f70f233d1.

--- testcases/python.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# Comment 1
-class Class(object):
-    """Docstring 1"""
-    def get(self):
-        """Docstring 2"""
-        # Comment 2 - pycco puts the function into the documntation not the code
-        datum = datetime.date.today()
