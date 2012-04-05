pycco-docs/pycco

commit e2032e19b05c052b8fad276a1fb6aac1a5b552a8

moved from table to pure css layout design

pycco_resources/__init__.py
@@ -22,7 +22,7 @@ h1, h2, h3, h4, h5, h6 {
 h2, h3, h4, h5, h6 {
     margin-top: 0;
   }
-#container {
+#container, div.section {
   position: relative;
 }
 #background {
@@ -32,95 +32,57 @@ h2, h3, h4, h5, h6 {
   border-left: 1px solid #e5e5ee;
   z-index: -1;
 }
-#jump_to, #jump_page {
-  background: white;
-  -webkit-box-shadow: 0 0 25px #777; -moz-box-shadow: 0 0 25px #777;
-  -webkit-border-bottom-left-radius: 5px; -moz-border-radius-bottomleft: 5px;
-  font: 10px Arial;
-  text-transform: uppercase;
-  cursor: pointer;
-  text-align: right;
+div.docs {
+  float: left;
+  max-width: 500px;
+  min-width: 500px;
+  min-height: 5px;
+  padding: 10px 25px 1px 50px;
+  vertical-align: top;
+  text-align: left;
 }
-#jump_to, #jump_wrapper {
-  position: fixed;
-  right: 0; top: 0;
-  padding: 5px 10px;
-}
-  #jump_wrapper {
-    padding: 0;
-    display: none;
+  .docs pre {
+    margin: 15px 0 15px;
+    padding-left: 15px;
   }
-    #jump_to:hover #jump_wrapper {
-      display: block;
+  .docs p tt, .docs p code {
+    background: #f8f8ff;
+    border: 1px solid #dedede;
+    font-size: 12px;
+    padding: 0 0.2em;
+  }
+  .octowrap {
+    position: relative;
+  }
+    .octothorpe {
+      font: 12px Arial;
+      text-decoration: none;
+      color: #454545;
+      position: absolute;
+      top: 3px; left: -20px;
+      padding: 1px 2px;
+      opacity: 0;
+      -webkit-transition: opacity 0.2s linear;
     }
-    #jump_page {
-      padding: 5px 0 3px;
-      margin: 0 0 25px 25px;
-    }
-      #jump_page .source {
-        display: block;
-        padding: 5px 10px;
-        text-decoration: none;
-        border-top: 1px solid #eee;
+      div.docs:hover .octothorpe {
+        opacity: 1;
       }
-        #jump_page .source:hover {
-          background: #f5f5ff;
-        }
-        #jump_page .source:first-child {
-        }
-table td {
-  border: 0;
-  outline: 0;
+div.code {
+  margin-left: 580px;
+  padding: 14px 15px 16px 50px;
+  vertical-align: top;
 }
-  td.docs, th.docs {
-    max-width: 500px;
-    min-width: 500px;
-    min-height: 5px;
-    padding: 10px 25px 1px 50px;
-    vertical-align: top;
-    text-align: left;
+  .code pre, .docs p code {
+    font-size: 12px;
   }
-    .docs pre {
-      margin: 15px 0 15px;
-      padding-left: 15px;
-    }
-    .docs p tt, .docs p code {
-      background: #f8f8ff;
-      border: 1px solid #dedede;
-      font-size: 12px;
-      padding: 0 0.2em;
-    }
-    .octowrap {
-      position: relative;
-    }
-      .octothorpe {
-        font: 12px Arial;
-        text-decoration: none;
-        color: #454545;
-        position: absolute;
-        top: 3px; left: -20px;
-        padding: 1px 2px;
-        opacity: 0;
-        -webkit-transition: opacity 0.2s linear;
-      }
-        td.docs:hover .octothorpe {
-          opacity: 1;
-        }
-  td.code, th.code {
-    padding: 14px 15px 16px 50px;
-    width: 100%;
-    vertical-align: top;
-    background: #f5f5ff;
-    border-left: 1px solid #e5e5ee;
-  }
-.code pre, .docs p code {
-  font-size: 12px;
-}
     pre, tt, code {
       line-height: 18px;
       font-family: Monaco, Consolas, "Lucida Console", monospace;
       margin: 0; padding: 0;
     }
+div.clearall {
+    clear: both;
+}
 
 
 /*---------------------- Syntax Highlighting -----------------------------*/
@@ -198,42 +160,22 @@ html = """\
   <link rel="stylesheet" href="{{ stylesheet }}">
 </head>
 <body>
+<div id="background"></div>
 <div id='container'>
-  <div id="background"></div>
-  {{#sources?}}
-  <div id="jump_to">
-    Jump To &hellip;
-    <div id="jump_wrapper">
-      <div id="jump_page">
-        {{#sources}}
-          <a class="source" href="{{ url }}">{{ basename }}</a>
-        {{/sources}}
+  {{#sections}}
+  <div id='section-{{ num }}' class='section'>
+    <div class='docs'>
+      <div class='octowrap'>
+        <a class='octothorpe' href='#section-{{ num }}'>#</a>
       </div>
+      {{{ docs_html }}}
+    </div>
+    <div class='code'>
+      {{{ code_html }}}
     </div>
   </div>
-  {{/sources?}}
-  <table cellspacing=0 cellpadding=0>
-  <thead>
-    <tr>
-      <th class=docs><h1>{{ title }}</h1></th>
-      <th class=code></th>
-    </tr>
-  </thead>
-  <tbody>
-    {{#sections}}
-    <tr id='section-{{ num }}'>
-      <td class=docs>
-        <div class="octowrap">
-          <a class="octothorpe" href="#section-{{ num }}">#</a>
-        </div>
-        {{{ docs_html }}}
-      </td>
-      <td class=code>
-        <div class='highlight'><pre>{{{ code_html }}}</pre></div>
-      </td>
-    </tr>
-    {{/sections}}
-  </table>
+  <div class='clearall'></div>
+  {{/sections}}
 </div>
 </body>
 """

commit 459f92f4956c793650dd582a68243899b2fc4b1f

Fix background not staying behind the code when scrolling horizontally
This commit fixes a bug introduced in e2032e19b05 which changed the
previous table layout to a pure CSS-based layout. Because the new layout
used a "position: fixed" background, the background would not scroll
when code lines were more wide than the browser window. This has been
fixed by making the background "position: absolute" and moving it into
the container DIV. Because the container DIV does not expand if its
content is wider than the browser window though, I had to set the body
background color to the same color than the code background and the
container background to white, so that the left (comment) part stays
white.

pycco_resources/__init__.py
@@ -6,6 +6,7 @@ body {
   line-height: 24px;
   color: #252519;
   margin: 0; padding: 0;
+  background: #f5f5ff;
 }
 a {
   color: #261a3b;
@@ -22,15 +23,18 @@ h1, h2, h3, h4, h5, h6 {
 h2, h3, h4, h5, h6 {
     margin-top: 0;
   }
+#container {
+  background: white;
+ }
 #container, div.section {
   position: relative;
 }
 #background {
-  position: fixed;
+  position: absolute;
   top: 0; left: 580px; right: 0; bottom: 0;
   background: #f5f5ff;
   border-left: 1px solid #e5e5ee;
-  z-index: -1;
+  z-index: 0;
 }
 #jump_to, #jump_page {
   background: white;
@@ -196,8 +200,8 @@ html = """\
   <link rel="stylesheet" href="{{ stylesheet }}">
 </head>
 <body>
-<div id="background"></div>
 <div id='container'>
+  <div id="background"></div>
   {{#sources?}}
   <div id="jump_to">
     Jump To &hellip;