239816deb0db2a3136b5442bca7422faf67d953d
Zach Smith <zach.smith@makespace.com>
2015-11-19 20:10:11 -0500

    Bump version number -> 0.3.1

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "Pycco",
-        version = "0.3.0",
-        description = """A Python port of Docco: the original quick-and-dirty,
+    name="Pycco",
+    version="0.3.1",
+    description="""A Python port of Docco: the original quick-and-dirty,
         hundred-line-long, literate-programming-style documentation generator.
         """,
-        author = "Nick Fitzgerald",
-        author_email = "fitzgen@gmail.com",
-        url = "http://fitzgen.github.com/pycco",
-        packages = find_packages(),
-        entry_points = {
-            'console_scripts': [
-                'pycco = pycco.main:main',
-                ]
-            },
-        install_requires = ['markdown', 'pygments', 'pystache', 'smartypants'],
-        extras_require = {'monitoring': 'watchdog'},
-        )
+    author="Nick Fitzgerald",
+    author_email="fitzgen@gmail.com",
+    url="http://fitzgen.github.com/pycco",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pycco = pycco.main:main',
+        ]
+    },
+    install_requires=['markdown', 'pygments', 'pystache', 'smartypants'],
+    extras_require={'monitoring': 'watchdog'},
+)
